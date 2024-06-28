a78c2fdfd4fbbe79d6d5bc7dc136f7c3e893bd23
AmaseCocoa <amase.cocoa@gmail.com>
2024-06-28 21:04:11 +0900

    開発環境の場合にエラーが出ないように

--- src/python_aid/__init__.py
+++ src/python_aid/__init__.py
@@ -1,3 +1,7 @@
 from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError
 
-__version__ = version("python-aid")
+try:
+    __version__ = version("python_aid")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
