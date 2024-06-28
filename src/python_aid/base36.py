4cb9df16e77546e67909e50fa5c845e8925384a7
AmaseCocoa <amase.cocoa@gmail.com>
2024-06-28 21:05:02 +0900

    base36パーサー関数名変更

--- src/python_aid/base36.py
+++ src/python_aid/base36.py
@@ -1,15 +1,20 @@
-def b36encode(number):
-    if not isinstance(number, (int, float)):
-        raise ValueError("must supply an integer or float")
+def encode(number: int) -> str:
+    if not isinstance(number, int):
+        raise TypeError('number must be an integer')
     if number < 0:
-        raise ValueError("must supply a non-negative number")
-    number = int(number)
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
-    base36 = ''
-    while number != 0:
-        number, i = divmod(number, 36)
-        base36 = alphabet[i] + base36
-    return base36 or alphabet[0]
+        raise ValueError('number must be non-negative')
 
-def b36decode(number):
+    alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+    base36 = ''
+
+    if 0 <= number < len(alphabet):
+        return alphabet[number]
+
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+
+    return base36
+
+def decode(number):
     return int(number, 36)
