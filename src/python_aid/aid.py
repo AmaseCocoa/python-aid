d68ac1ee93e37fcdfffb4b20d9a976056be5a89e
AmaseCocoa <amase.cocoa@gmail.com>
2024-06-28 21:04:49 +0900

    timestampの型ヒントをintからfloatに

--- src/python_aid/aid.py
+++ src/python_aid/aid.py
@@ -1,11 +1,12 @@
-import time
 import datetime
+import os
 import sys
+import time
 
 from python_aid import base36
-import os
 
-def get_noise() -> str:
+
+def getNoise() -> str:
     counter = int.from_bytes(os.urandom(2), 'little')
     return format(counter, 'x').zfill(2)[-2:]
 
@@ -16,21 +17,23 @@ def parseAid(aidx: str) -> datetime.datetime:
         str: aid
     """
     base36_time = aidx[:8]
-    time_milliseconds = int(base36.b36decode(base36_time))
+    time_milliseconds = int(base36.decode(base36_time))
     timestamp = 946684800 + time_milliseconds / 1000
     if sys.version_info < (3, 11): # Python3.11からdatetimee.UTCが追加されたため
         return datetime.datetime.utcfromtimestamp(timestamp)
     return datetime.datetime.fromtimestamp(timestamp, datetime.UTC)
 
-def genAid(timestamp: int=None) -> str:
+def genAid(timestamp: float=None) -> str:
     """aidを生成します。
 
     Returns:
         str: aid
     """
     if timestamp is None:
-        timestamp = int((time.time() - 946684800) * 1000)
-    base36_time = base36.b36encode(timestamp)
-    noise = get_noise()
+        timestamp = int((timestamp - 946684800) * 1000)
+        # raise NotImplementedError("The function to generate aid from timestamp is currently not available due to incompatibility with Misskey's aid :(")
+    timestamp = int((time.time() - 946684800) * 1000)
+    base36_time = base36.encode(timestamp)
+    noise = getNoise()
     aid = base36_time.zfill(8) + noise.zfill(2)
     return aid
